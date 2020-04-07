0f4bd8e8f397d2368d796326ef12764cb20dff55
Tarik <tj75467@gmail.com>
2020-04-07 17:04:43 -0400

    Animations on the text

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ var date = new Date();
 var year = date.getFullYear();
 
 document.querySelector(".copyright").innerHTML = "Copyright &copy; Tarik Jaber " + String(year);
+
+console.log("hello");
+
+const elements = $("h1, h2, h3, .bodytext p, .footer *, img");
+
+var windowHeight = window.innerHeight;;
+
+function checkPosition() {
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var positionFromTop = elements[i].getBoundingClientRect().top;
+
+    if (positionFromTop - windowHeight <= 0) {
+      element.classList.add('fade-in-element');
+      element.classList.remove('hidden');
+    }
+  }
+}
+
+document.addEventListener('scroll', function(e) {
+  checkPosition();
+});
+
+window.onload = function () {
+  checkPosition();
+}
