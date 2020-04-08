e59351a21c21a4a437a466b478718c7b6f079d62
Tarik <tj75467@gmail.com>
2020-04-07 23:18:46 -0400

    Updated animations

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ var year = date.getFullYear();
 
 document.querySelector(".copyright").innerHTML = "Copyright &copy; Tarik Jaber " + String(year);
 
-$("h2, .bodytext p, .footer *").addClass("hidden");
-const elements = $("h2, .bodytext p, .footer *");
+$("h2, .bodytext p").addClass("hidden");
+const elements = $("h2, .bodytext p");
 
-var windowHeight = window.innerHeight;;
+var windowHeight = window.innerHeight;
+var topFactor;
 
 function checkPosition() {
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     var positionFromTop = elements[i].getBoundingClientRect().top;
 
-    if (positionFromTop - windowHeight <= 0) {
-      element.classList.add('fade-in-element');
+    if (elements[i].parentNode.tagName === "FOOTER" || elements[i].parentNode.parentNode.tagName === "FOOTER") {
+      topFactor = 1.0;
+    } else {
+      topFactor = 0.80;
+    }
+
+    if (positionFromTop - topFactor * windowHeight <= 0) {
+      element.classList.add('fade-twist-element');
       element.classList.remove('hidden');
     }
+    
   }
 }
 
@@ -27,6 +35,6 @@ document.addEventListener('scroll', function(e) {
 window.onload = function () {
   checkPosition();
 
-  $(".title-column1 h1, .title-column1 h3").addClass("slide-in-left");
-  $(".title-column2 img").addClass("slide-in-right");
+  $(".title-column1").addClass("slide-in-left").removeClass("left-skewed");
+  $(".title-column2").addClass("slide-in-right").removeClass("right-skewed");
 }
