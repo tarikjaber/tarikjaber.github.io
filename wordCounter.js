061196f54d5d2ac85bde730d228a5248fac6ad66
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2022-04-08 12:24:18 -0400

    Updated text so that it autosaves

--- wordCounter.js
+++ wordCounter.js
@@ -2,15 +2,23 @@ const textArea = document.getElementById('text')
 const wordCount = document.getElementById('wordCount')
 const charCount = document.getElementById('charCount')
 
-textArea.addEventListener('input', function (e) {
-    let text = e.target.value
+let text = localStorage.getItem('text')
+textArea.value = text
+
+updateCounts();
+
+textArea.addEventListener('input', updateCounts);
+
+function updateCounts() {
+    text = textArea.value
+    localStorage.setItem('text', text)
 
     charCount.innerHTML = `Characters: ${text.length}`
 
-    let words = text.split(' ');
+    let words = text.split(' ')
     const results = words.filter((element) => {
         return element !== ''
     })
 
     wordCount.innerHTML = `Words: ${results.length}`
-})
+}
