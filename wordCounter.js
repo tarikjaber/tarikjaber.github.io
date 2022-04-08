a5c164e4bf2d64c6fbea9a4f5c6ddd39c9cf6665
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2022-04-08 11:53:12 -0400

    Added word counter

---
+++ wordCounter.js
@@ -0,0 +1,11 @@
+const textArea = document.getElementById('text')
+const wordCount = document.getElementById('wordCount')
+const charCount = document.getElementById('charCount')
+
+textArea.addEventListener('input', function (e) {
+    console.log('hello world')
+    let text = e.target.value
+
+    charCount.innerHTML = `Characters: ${text.length}`
+    wordCount.innerHTML = `Words: ${text.split(' ').length}`
+})
