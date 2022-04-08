5b9ebd01253dea01ac95c07ef760271a05db47ba
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2022-04-08 16:45:13 -0400

    Finalized word counter

--- wordCounter.js
+++ wordCounter.js
@@ -1,13 +1,15 @@
 const textArea = document.getElementById('text')
 const wordCount = document.getElementById('wordCount')
 const charCount = document.getElementById('charCount')
+const copyBtn = document.getElementById('copy')
+const copiedText = document.getElementById('copied')
 
 let text = localStorage.getItem('text')
 textArea.value = text
 
-updateCounts();
+updateCounts()
 
-textArea.addEventListener('input', updateCounts);
+textArea.addEventListener('input', updateCounts)
 
 function updateCounts() {
     text = textArea.value
@@ -22,3 +24,14 @@ function updateCounts() {
 
     wordCount.innerHTML = `Words: ${results.length}`
 }
+
+copyBtn.addEventListener('click', () => {
+    copyBtn.style.backgroundColor = 'mediumseagreen'
+    copyBtn.innerHTML = 'Copied!'
+    navigator.clipboard.writeText(textArea.value)
+    setTimeout(() => {
+        copyBtn.innerHTML = 'Copy Text'
+        copyBtn.style.color = 'white'
+        copyBtn.style.backgroundColor = 'dodgerblue'
+    }, 500)
+})
